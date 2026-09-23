0361797a8f2f13e2a73f18c93709746c88f82da3
Hermann-123 <degbegnonhermann096@gmail.com>
2026-09-23 11:20:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -38,13 +38,11 @@ from threading import Thread, Timer
 # CONFIGURATION PRINCIPALE ET SÉCURITÉ
 # ==========================================
 
-TELEGRAM_TOKEN = "8658287331:AAE1AD_2WVMsMb4OpIiGMbRoiGUAnI8uYFk"
+TELEGRAM_TOKEN = "8658287331:AAGJdGx_qaacvKlx8R7XR5A5l_Aa6zOJ0i4"
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
-
 ADMIN_ID = 5968288964
 CAPITAL_ACTUEL = 40650
 FMP_API_KEY = os.environ.get("FMP_API_KEY", "D0srw6sB3otYTc00UdBE9otPIbhkKV8X")
-
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY", "").strip()
 GROQ_MODEL = "llama-3.1-8b-instant"
 GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
